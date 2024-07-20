05b6f4e0d227051f1929e21fe9e048c0f662e09e
siddhantOptacloud <siddhant@optacloud.in>
2024-07-20 21:26:15 +0530

    attendance intergated for student

--- src/Modules/Student/Attendance/AttendanceMain.js
+++ src/Modules/Student/Attendance/AttendanceMain.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AttendanceSummary from './AttendanceSummary';
 import CalendarHeader from './Calender';
 import StudentDashLayout from '../../../Components/Student/StudentDashLayout';
+import axios from 'axios';
+import moment from 'moment';
 
 
 const AttendanceMain = () => {
-    const handlePreviousMonth = () => {
-        // Logic for previous month
-    };
 
-    const handleNextMonth = () => {
-        // Logic for next month
+    const [attendanceData, setAttendanceData] = useState({})
+    const [currentDate, setCurrentDate] = useState(moment());
+    const [summary, setSummary] = useState({ presentCount: 0, absentCount: 0, leaveCount: 0 });
+
+    const fetchAttendance = async (month, year) => {
+        try {
+            const token = localStorage.getItem('student:token')
+            if (!token) {
+                throw new Error('Authentication not found');
+            }
+
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/studentDashboard/myAttendance`, {
+                params: { month, year },
+                headers: {
+                    'Authentication': token
+                }
+            })
+
+            const { report, summary } = response.data.report;
+            const attendanceMap = report.reduce((acc, entry) => {
+                acc[entry.date] = entry.status;
+                return acc;
+            }, {});
+            setAttendanceData(attendanceMap);
+            setSummary(summary);
+
+        } catch (error) {
+            console.error("Failed to fetch Attendance:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchAttendance(currentDate.month() + 1, currentDate.year());
+    }, [currentDate])
+
+    const onPanelChange = (value) => {
+        setCurrentDate(value);
     };
 
     return (
         <StudentDashLayout>
             <div className="container mx-auto py-4">
-                <AttendanceSummary present={250} absent={116} leave={16} />
+                <AttendanceSummary present={summary.presentCount} absent={summary.absentCount} leave={summary.leaveCount} />
                 <div className='border-b border-t border-gray-200 my-4 p-4'>
-                    <CalendarHeader month="March" year="2024" onPreviousMonth={handlePreviousMonth} onNextMonth={handleNextMonth} />
+                    <CalendarHeader attendanceData={attendanceData}
+                        onPanelChange={onPanelChange} />
                 </div>
             </div>
         </StudentDashLayout>
