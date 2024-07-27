44693ce658312bfd624ee6e548bb9e41b65c4372
Amanstudentdiwan <amandubey8833@gmail.com>
2024-07-27 22:09:33 +0530

    Optimization | Aman

--- src/Components/Admin/SideMenubar.js
+++ src/Components/Admin/SideMenubar.js
@@ -186,7 +186,7 @@ const SideMenubar = () => {
             <div className="ml-4">
               <h2 className="text-sm font-semibold">
                 {/* {userDetails.fullName || "Aman Dubey"} */}
-                {userDetails.adminName || "User Name"}
+                {userDetails.adminName || "User "}
               </h2>
               <p className="text-gray-500 capitalize">{role}</p>
             </div>
