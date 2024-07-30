d652e1d7040131c271070ddd359c6810b49e0313
Amanstudentdiwan <amandubey8833@gmail.com>
2024-07-30 12:44:37 +0530

    Student Id Card | Admin Panel | Aman

--- src/Modules/Admin/Addmission/Components/StudentCard.js
+++ src/Modules/Admin/Addmission/Components/StudentCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { GiArmorUpgrade } from "react-icons/gi";
 import CardBanner from "../../../../Assets/AdmissionCard/CardBanner.jpg";
+import Logo from "../../../../Components/Common/Logo";
 
 const StudentCard = ({ studentInfo, imagePreview }) => {
   const {
@@ -14,17 +15,21 @@ const StudentCard = ({ studentInfo, imagePreview }) => {
     email,
   } = studentInfo;
 
+  // Truncate email if it is longer than 20 characters
+  const truncateEmail = (email) => {
+    return email.length > 20 ? email.substring(0, 20) + "..." : email;
+  };
+
   return (
     <div className="pb-4 mt-2 bg-white rounded-lg shadow-md w-64 border">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col ">
         <div
           className="w-full h-40 bg-cover bg-center rounded-t-md relative flex justify-center items-center"
           style={{ backgroundImage: `url(${CardBanner})` }}
         >
           <div className="absolute top-2 left-2 text-white text-sm font-semibold">
             <div className="flex items-center space-x-1">
-              <GiArmorUpgrade className="w-5 h-5 text-pink-600" />
-              <span>Student Diwan</span>
+              <Logo height="h-6" />
             </div>
           </div>
           <div className="mt-4 bg-white rounded-full flex justify-center items-center">
@@ -35,11 +40,11 @@ const StudentCard = ({ studentInfo, imagePreview }) => {
             />
           </div>
         </div>
-        <div className="mt-4 text-center">
-          <h2 className="text-lg font-semibold">
+        <div className="mt-4  px-4">
+          <h2 className="text-lg text-center font-semibold">
             {firstName + " " + lastName || "Student Name"}
           </h2>
-          <div className="text-sm text-gray-600 mt-2 space-y-1 text-left">
+          <div className="text-sm text-gray-600 mt-2 space-y-1 ">
             <p>
               <span className="font-semibold">ID No</span>: {studentId}
             </p>
@@ -55,8 +60,9 @@ const StudentCard = ({ studentInfo, imagePreview }) => {
             <p>
               <span className="font-semibold">Nationality</span>: {nationality}
             </p>
-            <p>
-              <span className="font-semibold">Email</span>: {email}
+            <p title={email}>
+              <span className="font-semibold">Email</span>:{" "}
+              {truncateEmail(email)}
             </p>
           </div>
         </div>
