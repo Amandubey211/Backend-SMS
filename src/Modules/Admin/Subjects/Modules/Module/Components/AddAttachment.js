d05bc17e292e59e671c40d4163b3464b0714cc1c
Amanstudentdiwan <amandubey8833@gmail.com>
2024-08-12 12:10:45 +0530

    Quiz Issue Fixed | Admin Panel | Aman

---
+++ src/Modules/Admin/Subjects/Modules/Module/Components/AddAttachment.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const AddAttachment = ({ chapterData, isEditing, onClose, fetchModules }) => {
+  return <>Add Attachment </>;
+};
+
+export default AddAttachment;
