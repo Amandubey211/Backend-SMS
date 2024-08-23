5101efce3a3108df73d7c7893ebd52d35858fe6b
siddhantOptacloud <siddhant@optacloud.in>
2024-08-23 14:20:01 +0530

    changes made in library dashboard

---
+++ src/Hooks/AuthHooks/Staff/Admin/LibraryBooks/useGetFilteredIssueBooks.js
@@ -0,0 +1,41 @@
+import { useState, useCallback, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../../../../../config/Common";
+
+const useGetFilteredIssueBooks = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [books, setBooks] = useState([]);
+
+  const role = useSelector((store) => store.Auth.role);
+
+  const fetchBooks = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const token = localStorage.getItem(`${role}:token`);
+      const response = await axios.get(`${baseUrl}/admin/all/bookIssue`, {
+        headers: { Authentication: token },
+      });
+
+      console.log('Fetched Books:', response.data.books); 
+      setBooks(response.data.books);
+      setLoading(false);
+    } catch (err) {
+      const errorMessage = err.response?.data?.message || "Failed to fetch books";
+      toast.error(errorMessage);
+      setLoading(false);
+      setError(errorMessage);
+    }
+  }, [role, baseUrl]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
+
+  return { loading, error, books };
+};
+
+export default useGetFilteredIssueBooks;
