3fd82f760546f5ab7b855e0c8296263e8e6d34f3
Amanstudentdiwan <amandubey8833@gmail.com>
2024-09-04 11:29:27 +0530

    Parent Fixed | Aman

--- src/Modules/Parents/Dasboard/ParentDash.js
+++ src/Modules/Parents/Dasboard/ParentDash.js
@@ -3,9 +3,7 @@ import ParentSection from "./MainSection.js";
 import Layout from "../../../Components/Common/Layout.js";
 import DashLayout from "../../../Components/Parents/ParentDashLayout.js";
 
-import useNavHeading from "../../../Hooks/CommonHooks/useNavHeading .js";
 const ParentDash = () => {
-  useNavHeading("Main Dashboard");
   return (
     <Layout title="Parents | Dashboard">
       <DashLayout children={<ParentSection />} hideAvatarList={true} />
