a1e485b03bf91bfb056e839626b1051086a91fdb
TechTeam-studentdiwan <tech@studentdiwan.com>
2024-10-11 22:06:26 +0530

    add assigment

---
+++ src/Store/Slices/Student/MyClass/Class/Subjects/Assignment/assignment.action.js
@@ -0,0 +1,64 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { setShowError } from "../../../../../Common/Alerts/alertsSlice";
+import { ErrorMsg } from "../../../../../Common/Alerts/errorhandling.action";
+
+
+
+export const stdGetAssignment = createAsyncThunk(
+    'assignment/stdGetAssignment',
+    async (_, { rejectWithValue }) => {
+
+        const token = localStorage.getItem("student:token");
+        if (!token) {
+            dispatch(setShowError(true));
+            return rejectWithValue('Authentication failed!');
+        }
+
+        try {
+            dispatch(setShowError(false));
+            const res = await axios.get(`${baseUrl}/`, {
+                headers: {
+                    Authentication: token
+                }
+            });
+
+            const data = res?.data;
+            console.log(data);
+        } catch (error) {
+            console.log("Error in student class teacher", error);
+            const err = ErrorMsg(error);
+            dispatch(setShowError(true));
+            return rejectWithValue(err.message);
+        }
+    }
+) 
+
+export const stdDoAssignment = createAsyncThunk(
+    'assignment/stdDoAssignment',
+    async ({}, { rejectWithValue }) => {
+
+        const token = localStorage.getItem("student:token");
+        if (!token) {
+            dispatch(setShowError(true));
+            return rejectWithValue('Authentication failed!');
+        }
+
+        try {
+            dispatch(setShowError(false));
+            const res = await axios.put(`${baseUrl}/`,{
+                headers: {
+                    Authentication: token
+                }
+            });
+
+            const data = res?.data;
+            console.log(data);
+        } catch (error) {
+            console.log("Error in student class teacher", error);
+            const err = ErrorMsg(error);
+            dispatch(setShowError(true));
+            return rejectWithValue(err.message);
+        }
+    }
+) 
