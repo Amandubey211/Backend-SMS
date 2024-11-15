8b34011b7b721fd6935b054480ce9c813fb8fe56
OptimusPrimeRahul <rahul@studentdiwan.com>
2024-11-15 15:09:15 +0530

    Module UI Fix

--- src/Modules/Student/StudentClass/Subjects/Modules/Module/Components/ModuleCard.js
+++ src/Modules/Student/StudentClass/Subjects/Modules/Module/Components/ModuleCard.js
@@ -10,29 +10,21 @@ const ModuleCard = ({
 }) => {
   return (
     <div
-      className={`relative mb-4 border ${
-        isSelected ? "border-2 border-rose-400" : ""
-      } bg-white rounded-lg cursor-pointer`}
+      className={`relative mb-4 border ${isSelected ? "border-2 border-rose-400" : ""
+        } bg-white rounded-lg cursor-pointer`}
       onClick={onSelect}
     >
       <img
         src={imageUrl}
         alt={title}
-        className="w-full h-36 object-cover rounded-t-lg"
+        className="w-full h-32 object-cover rounded-t-lg" // Reduced height of the image
       />
-      <div className="p-4">
+      <div className="p-3"> {/* Reduced padding here */}
         <h2 className="font-semibold text-lg">{title}</h2>
-        <div className="flex justify-between items-center mt-2">
-          <p className="bg-gradient-to-r from-pink-100 to-purple-200 font-semibold rounded-full py-1 px-4">
+        <div className="flex justify-between items-center mt-1"> {/* Reduced margin */}
+          <p className="bg-gradient-to-r from-pink-100 to-purple-200 font-semibold rounded-full py-1 px-3"> {/* Adjusted padding */}
             <span className="text-gradient">Module {moduleNumber}</span>
           </p>
-          {/* <div className="flex items-center space-x-2">
-            {isCompleted ? (
-              <span className="text-green-500">Completed</span>
-            ) : (
-              <span className="text-gray-500">In Progress</span>
-            )}
-          </div> */}
         </div>
       </div>
     </div>
