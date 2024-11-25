140c978349e73b869873b54241dde251a7678ef9
kingdomcollectionsaws <kingdomcollectionsaws@gmail.com>
2024-11-25 10:28:24 +0530

    api service in user

--- src/Store/Slices/Common/AcademicYear/academicYear.action.js
+++ src/Store/Slices/Common/AcademicYear/academicYear.action.js
@@ -1,116 +1,69 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { baseUrl } from "../../../../config/Common";
-import { ErrorMsg } from "../../Common/Alerts/errorhandling.action";
+import { ErrorMsg, handleError } from "../../Common/Alerts/errorhandling.action";
 import { setShowError } from "../../Common/Alerts/alertsSlice";
 import { setActiveAcademicYear, setSeletedAcademicYear } from "./academicYear.slice";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { deleteData, getData, postData, putData } from "../../../../services/apiEndpoints";
 
 export const fetchAcademicYear = createAsyncThunk("user/AcademicYear", async (_, { rejectWithValue, dispatch, getState }) => {
 
-  const token = getState().common.auth.token;
 
-  if (!token) {
-    dispatch(setShowError(true));
-    return rejectWithValue(`Authentication failed!`);
-  }
- const say = localStorage.getItem('say')
+
   try {
     dispatch(setShowError(false));
-
-    const res = await axios.get(`${baseUrl}/admin/getAllAcademicYear`, {
-      headers: { Authentication: `Bearer ${token}` },
-    });
-    if(res.data.success){
-     const Ay = res.data?.data.find((i)=>i._id == say );
-console.log('ayyyy',Ay);
-        setSeletedAcademicYear(Ay)
+    const say = localStorage.getItem('say')
+    const res = await getData(`/admin/getAllAcademicYear`);
+    if (res.success) {
+      const Ay = res?.data.find((i) => i._id == say);
+      setSeletedAcademicYear(Ay)
     }
-    return res?.data?.data
+    return res?.data
   } catch (error) {
-    console.log("Error in academic Year", error);
-    const err = ErrorMsg(error);
-    dispatch(setShowError(true));
-    return rejectWithValue(err.message);
+    return handleError(error, dispatch, rejectWithValue);
   }
 
 });
 
 export const updateAcademicYear = createAsyncThunk("user/updateAcademicYear", async ({ id, data }, { rejectWithValue, dispatch, getState }) => {
 
-  const token = getState().common.auth.token;
-
-  if (!token) {
-    dispatch(setShowError(true));
-    return rejectWithValue(`Authentication failed!`);
-  }
-
   try {
     dispatch(setShowError(false));
 
-    const res = await axios.put(`${baseUrl}/admin/updateAcademicYear/${id}`, data, {
-      headers: { Authentication: `Bearer ${token}` },
-    });
+    const res = await putData(`/admin/updateAcademicYear/${id}`, data);
     toast.success("Academic year updated successfully.");
     dispatch(fetchAcademicYear())
-    return res?.data?.data
+    return res?.data
   } catch (error) {
-    console.log("Error in  update academic Year", error);
-    const err = ErrorMsg(error);
-    dispatch(setShowError(true));
-    return rejectWithValue(err.message);
+    return handleError(error, dispatch, rejectWithValue);
   }
 
 });
-export const addAcademicYear = createAsyncThunk("user/addAcademicYear", async ( data , { rejectWithValue, dispatch, getState }) => {
-
-  const token = getState().common.auth.token;
-
-  if (!token) {
-    dispatch(setShowError(true));
-    return rejectWithValue(`Authentication failed!`);
-  }
-
+export const addAcademicYear = createAsyncThunk("user/addAcademicYear", async (data, { rejectWithValue, dispatch, getState }) => {
   try {
     dispatch(setShowError(false));
 
-    const res = await axios.post(`${baseUrl}/admin/createAcademicYear`, data, {
-      headers: { Authentication: `Bearer ${token}` },
-    });
+    const res = await postData(`/admin/createAcademicYear`, data);
     toast.success("Academic year created successfully.");
     dispatch(fetchAcademicYear())
-    return res?.data
+    return res
   } catch (error) {
 
-    const err = ErrorMsg(error);
-    dispatch(setShowError(true));
-    return rejectWithValue(err.message);
+    return handleError(error, dispatch, rejectWithValue);
   }
 
 });
-export const deleteAcademicYear = createAsyncThunk("user/deleteAcademicYear", async ( id , { rejectWithValue, dispatch, getState }) => {
-
-  const token = getState().common.auth.token;
-
-  if (!token) {
-    dispatch(setShowError(true));
-    return rejectWithValue(`Authentication failed!`);
-  }
+export const deleteAcademicYear = createAsyncThunk("user/deleteAcademicYear", async (id, { rejectWithValue, dispatch, getState }) => {
 
   try {
     dispatch(setShowError(false));
-
-    const res = await axios.delete(`${baseUrl}/admin/deleteAcademicYear/${id}`, {
-      headers: { Authentication: `Bearer ${token}` },
-    });
+    const res = await deleteData(`/admin/deleteAcademicYear/${id}`);
     toast.success("Academic year deleted successfully.");
     dispatch(fetchAcademicYear())
-    return res?.data
+    return res
   } catch (error) {
-
-    const err = ErrorMsg(error);
-    dispatch(setShowError(true));
-    return rejectWithValue(err.message);
+    return handleError(error, dispatch, rejectWithValue);
   }
 
 });
