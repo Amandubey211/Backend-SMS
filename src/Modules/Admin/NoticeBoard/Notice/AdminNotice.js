dd70e9349a4ec7ba71a4edba8ab613903ee2422c
Rahul Vishwakarma <karmarahul67@gmail.com>
2025-01-27 21:06:51 +0530

    RBAC Permission updates V2

--- src/Modules/Admin/NoticeBoard/Notice/AdminNotice.js
+++ src/Modules/Admin/NoticeBoard/Notice/AdminNotice.js
@@ -111,16 +111,16 @@ const AdminNotice = () => {
               role === "finance" ||
               role === "teacher" ||
               role === "librarian") && (
-                 <ProtectedAction requiredPermission={PERMISSIONS.ADD_NEW_NOTICE}>
-              <button
-                className="flex items-center justify-center border border-transparent bg-gradient-to-r from-pink-500 to-purple-500 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 transform hover:scale-105 hover:shadow-xl"
-                onClick={() => setSidebarOpen(true)} // Open sidebar
-              >
-                <FiPlus className="mr-2" />
-                {t("Add Notice")}
-              </button>
-              </ProtectedAction>
-            )}
+                <ProtectedAction requiredPermission={PERMISSIONS.ADD_NEW_NOTICE}>
+                  <button
+                    className="flex items-center justify-center border border-transparent bg-gradient-to-r from-pink-500 to-purple-500 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 transform hover:scale-105 hover:shadow-xl"
+                    onClick={() => setSidebarOpen(true)} // Open sidebar
+                  >
+                    <FiPlus className="mr-2" />
+                    {t("Add Notice")}
+                  </button>
+                </ProtectedAction>
+              )}
           </div>
 
           <ProtectedSection requiredPermission={PERMISSIONS.SHOW_NOTICES} title={'Notices'}>
