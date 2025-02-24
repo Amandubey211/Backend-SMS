6daf9a3a1aae216e8aa1c76a927dd4cb6d7c0788
Amanstudentdiwan <amandubey8833@gmail.com>
2025-02-24 18:29:56 +0530

    Fix + MOD + OPT : Finance Studnet Fees Module Search Bar | Admin Panel | Aman

--- src/Store/Slices/Admin/TimeTable/timetable.action.js
+++ src/Store/Slices/Admin/TimeTable/timetable.action.js
@@ -3,7 +3,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { setShowError } from "../../Common/Alerts/alertsSlice";
 import { handleError } from "../../Common/Alerts/errorhandling.action";
 import { getAY } from "../../../../Utils/academivYear";
-import { getData, postData, putData, deleteData } from "../../../../services/apiEndpoints";
+import {
+  getData,
+  postData,
+  putData,
+  deleteData,
+} from "../../../../services/apiEndpoints";
 import { getUserRole } from "../../../../Utils/getRoles";
 
 // Fetch Timetables
@@ -11,49 +16,56 @@ export const fetchTimetables = createAsyncThunk(
   "timetable/fetchTimetables",
   async (filters = {}, { rejectWithValue, getState, dispatch }) => {
     const { role } = getState().common.auth;
-   
-
     let updatedFilters = { ...filters };
 
-    // If role is student, include classId from localStorage
+    // If role is student or teacher, include classId from state
     if (role === "student" || role === "teacher") {
-      const {userDetails} = getState((store) => store.common.user);
-      const classId = userDetails?.classId
+      const { userDetails } = getState().common.user;
+      const classId = userDetails?.classId;
       if (classId) {
         updatedFilters = { ...updatedFilters, classId };
       }
     }
 
+    // Add semesterId from state
+    const semesterId = getState().common.user.classInfo.selectedSemester.id;
+    if (semesterId) {
+      updatedFilters = { ...updatedFilters, semesterId };
+    }
+
     try {
       const say = getAY();
       const getRole = getUserRole(getState);
-      dispatch(setShowError(false))
-      const response = await getData(`/${getRole}/timetable?say=${say}`, updatedFilters);
-
-      // Ensure you're accessing the correct data property
-      return response?.data; // Accessing the nested data property
+      dispatch(setShowError(false));
+      const response = await getData(
+        `/${getRole}/timetable?say=${say}`,
+        updatedFilters
+      );
+      return response?.data;
     } catch (error) {
-
-        return handleError(error, dispatch, rejectWithValue);
+      return handleError(error, dispatch, rejectWithValue);
     }
   }
 );
-
 
 // Create Timetable
 export const createTimetable = createAsyncThunk(
   "timetable/createTimetable",
   async (data, { rejectWithValue, getState, dispatch }) => {
-    
-
     try {
-      const say = getAY(); 
+      const semesterId = getState().common.user.classInfo.selectedSemester.id;
+      // Merge semesterId into the request body
+      data = { ...data, semesterId };
+      const say = getAY();
       const getRole = getUserRole(getState);
-      dispatch(setShowError(false)); // Explicitly reset error visibility
-      const response = await postData(`/${getRole}/create-timetable?say=${say}`, data); // Use postData from apiEndpoints
-      return response?.data; // Directly returning the response
+      dispatch(setShowError(false));
+      const response = await postData(
+        `/${getRole}/create-timetable?say=${say}`,
+        data
+      );
+      return response?.data;
     } catch (error) {
-      return handleError(error, dispatch, rejectWithValue); // Use centralized error handling
+      return handleError(error, dispatch, rejectWithValue);
     }
   }
 );
@@ -62,16 +74,20 @@ export const createTimetable = createAsyncThunk(
 export const updateTimetable = createAsyncThunk(
   "timetable/updateTimetable",
   async ({ id, data }, { rejectWithValue, getState, dispatch }) => {
-    
-
     try {
-      const say = getAY(); 
+      const semesterId = getState().common.user.classInfo.selectedSemester.id;
+      // Merge semesterId into the request body
+      data = { ...data, semesterId };
+      const say = getAY();
       const getRole = getUserRole(getState);
-      dispatch(setShowError(false)); // Explicitly reset error visibility
-      const response = await putData(`/${getRole}/update-timetable/${id}?say=${say}`, data); // Use putData from apiEndpoints
-      return response?.data; // Access the nested data property using optional chaining
+      dispatch(setShowError(false));
+      const response = await putData(
+        `/${getRole}/update-timetable/${id}?say=${say}`,
+        data
+      );
+      return response?.data;
     } catch (error) {
-      return handleError(error, dispatch, rejectWithValue); // Use centralized error handling
+      return handleError(error, dispatch, rejectWithValue);
     }
   }
 );
@@ -81,13 +97,13 @@ export const deleteTimetable = createAsyncThunk(
   "timetable/deleteTimetable",
   async (id, { rejectWithValue, getState, dispatch }) => {
     try {
-      const say = getAY(); 
+      const say = getAY();
       const getRole = getUserRole(getState);
-      dispatch(setShowError(false)); // Explicitly reset error visibility
-      await deleteData(`/${getRole}/delete-timetable/${id}?say=${say}`); // Use deleteData from apiEndpoints
-      return { id }; // Returning ID of the deleted timetable
+      dispatch(setShowError(false));
+      await deleteData(`/${getRole}/delete-timetable/${id}?say=${say}`);
+      return { id };
     } catch (error) {
-      return handleError(error, dispatch, rejectWithValue); // Use centralized error handling
+      return handleError(error, dispatch, rejectWithValue);
     }
   }
 );
