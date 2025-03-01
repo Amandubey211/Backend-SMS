52e54e45ed1c492aa7c5429a38e51b4a57a17210
Amanstudentdiwan <amandubey8833@gmail.com>
2025-03-02 01:05:01 +0530

    Fixed : Addmission toast fixed when error

--- src/services/apiEndpoints.js
+++ src/services/apiEndpoints.js
@@ -34,12 +34,7 @@ export const putData = async (endpoint, data) => {
     const response = await apiService.put(endpoint, data);
     return response?.data;
   } catch (error) {
-    console.log("error", error);
-    if (error.response && error.response.data) {
-      console.log(error.response.data, "sdf");
-
-      return error.response.data;
-    }
+    return error;
   }
 };
 
