9f3dd3269bdffd38c0ee6f97de42d7ba799262cd
DYNOTAWFEEQ <tawfeeq@studentdiwan.com>
2025-05-08 12:15:11 +0530

    pending forms

---
+++ src/Modules/Admin/Verification/PendingForms.js
@@ -0,0 +1,91 @@
+// src/Modules/Admin/Verification/PendingForms.js
+
+import React, { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import UnVerifiedStudentCard from "./UnVerifiedStudentCard";
+import { fetchUnverifiedStudents } from "../../../Store/Slices/Admin/Verification/VerificationThunks";
+import Spinner from "../../../Components/Common/Spinner";
+import { FaUserSlash } from "react-icons/fa"; // Import a suitable icon
+import { useTranslation } from "react-i18next";
+import ProtectedSection from "../../../Routes/ProtectedRoutes/ProtectedSection";
+import ProtectedAction from "../../../Routes/ProtectedRoutes/ProtectedAction";
+import { PERMISSIONS } from "../../../config/permission";
+
+const CACHE_DURATION = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+const PendingForms = () => {
+  const dispatch = useDispatch();
+  const { t } = useTranslation("admVerification");
+  const {
+    unVerifiedStudents,
+    loadingUnverified,
+    searchQuery,
+    lastFetchedUnverified,
+  } = useSelector((state) => state.admin.verification);
+
+  // Fetch unverified students on mount or if data is stale
+  useEffect(() => {
+    // const now = Date.now();
+    // const isDataStale =
+    //   !lastFetchedUnverified || now - lastFetchedUnverified > CACHE_DURATION;
+
+    // if (unVerifiedStudents?.length === 0 || isDataStale) {
+    dispatch(fetchUnverifiedStudents({formStatus:"draft"}));
+    //dispatch, unVerifiedStudents?.length, lastFetchedUnverified
+    // }
+  }, []);
+
+  // Memoized filtered students based on search query
+  const filteredStudents = useMemo(() => {
+    if (!searchQuery) return unVerifiedStudents;
+    return unVerifiedStudents.filter((student) => {
+      const fullName = `${student.firstName} ${student.lastName}`.toLowerCase();
+      const email = student.email.toLowerCase();
+      const query = searchQuery.toLowerCase();
+      return email.includes(query) || fullName.includes(query);
+    });
+  }, [searchQuery, unVerifiedStudents]);
+
+  if (loadingUnverified) {
+    return (
+      <div className="text-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (filteredStudents?.length === 0) {
+    return (
+      <ProtectedSection
+        requiredPermission={PERMISSIONS.SHOW_UNVERIFIED_STUDENTS}
+        title={t("Unverified Students")}
+      >
+        <div className="flex flex-col justify-center items-center h-64">
+          <FaUserSlash className="text-6xl text-gray-400 mb-4" />{" "}
+          {/* Big Icon */}
+          <p className="text-center text-gray-500 text-xl">
+            {t("No Pending Form found.")}
+          </p>{" "}
+          {/* Text below icon */}
+        </div>
+      </ProtectedSection>
+    );
+  }
+
+  return (
+    <div className="animate-fadeIn">
+      <ProtectedSection
+        requiredPermission={PERMISSIONS.SHOW_UNVERIFIED_STUDENTS}
+        title={t("Unverified Students")}
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredStudents?.map((student) => (
+            <UnVerifiedStudentCard key={student._id} studentId={student._id} />
+          ))}
+        </div>
+      </ProtectedSection>
+    </div>
+  );
+};
+
+export default PendingForms;
