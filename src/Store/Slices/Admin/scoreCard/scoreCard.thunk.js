d98490b48b2e626723515c7d33027c865fe65cd3
DYNOTAWFEEQ <tawfeeq@studentdiwan.com>
2025-05-22 11:02:46 +0530

    scorecard

--- src/Store/Slices/Admin/scoreCard/scoreCard.thunk.js
+++ src/Store/Slices/Admin/scoreCard/scoreCard.thunk.js
@@ -9,6 +9,7 @@ import { getAY } from "../../../../Utils/academivYear";
 import { getUserRole } from "../../../../Utils/getRoles";
 import { handleError } from "../../Common/Alerts/errorhandling.action";
 import { setShowError } from "../../Common/Alerts/alertsSlice";
+import toast from "react-hot-toast";
 
 export const addScoreCard = createAsyncThunk(
   "scoreCard/addScoreCard",
@@ -18,6 +19,25 @@ export const addScoreCard = createAsyncThunk(
       const role = getUserRole(getState);
       dispatch(setShowError(false));
       const response = await postData(`/${role}/scoreCard/add?say=${say}`, data);
+      return response;
+    } catch (error) {
+      return handleError(error, dispatch, rejectWithValue);
+    }
+  }
+);
+export const addScoreCardCellData = createAsyncThunk(
+  "scoreCard/addScoreCardCellData",
+  async (data, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const say = getAY();
+      const role = getUserRole(getState);
+      dispatch(setShowError(false));
+      const response = await putData(`/${role}/scoreCard/add/cell/${data.classId}?say=${say}`, data);
+      if(response.success){
+        toast.success("Added Successfully");
+      }else{
+        toast.error("Cell Number is already Used");
+      }
       return response;
     } catch (error) {
       return handleError(error, dispatch, rejectWithValue);
