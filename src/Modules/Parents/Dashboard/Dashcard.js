3ad063cd8f3b19c6315efb36889fe5c2a5583794
Devansh-1208 <devansh@studentdiwan.com>
2025-06-20 15:44:54 +0530

    Replace Dummy Data of Student Fees on Parent Panel

--- src/Modules/Parents/Dashboard/Dashcard.js
+++ src/Modules/Parents/Dashboard/Dashcard.js
@@ -6,11 +6,11 @@ import { CiMoneyBill } from "react-icons/ci";
 import { FaBell } from "react-icons/fa"; // for the bell icon
 import { useTranslation } from "react-i18next";
 import { ParentDashcard } from "../Skeletons.js";
-
+import { useNavigate } from "react-router-dom";
 const DashCard = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
-
+  const navigate = useNavigate()
   // Get data from Redux store
   const { cardsData = {}, loading } = useSelector((state) => state?.Parent?.dashboard || {});
   const [isLoading, setIsLoading] = useState(true);
@@ -27,19 +27,20 @@ const DashCard = () => {
 
   // Card configuration for the UI
   const cardData = [
-    {
-      label: t("Upcoming Exam", { ns: "stdFinance" }),
-      value: cardsData?.upcomingExamsCount?.toString() || "0",
-      cardBg: "bg-violet-100",  // Pastel background
-      hexBg: "bg-violet-500",   // Darker color for hex shape
-      icon: <RiBookOpenLine />,
-    },
+    // {
+    //   label: t("Upcoming Assignments & Quizzes", { ns: "stdFinance" }),
+    //   value: cardsData?.upcomingExamsCount?.toString() || "0",
+    //   cardBg: "bg-violet-100",  // Pastel background
+    //   hexBg: "bg-violet-500",   // Darker color for hex shape
+    //   icon: <RiBookOpenLine />,
+    // },
     {
       label: t("Due Fees", { ns: "stdFinance" }),
       value: cardsData?.dueFees?.toString() || "0",
       cardBg: "bg-rose-100",
       hexBg: "bg-rose-500",
       icon: <CiMoneyBill />,
+      path: '/parentfinance'
     },
     {
       label: t("Upcoming Event", { ns: "stdFinance" }),
@@ -47,6 +48,7 @@ const DashCard = () => {
       cardBg: "bg-blue-100",
       hexBg: "bg-blue-500",
       icon: <RiCalendarCheckLine />,
+      path: '/parent/events'
     },
     {
       label: t("Upcoming Notice", { ns: "stdFinance" }),
@@ -54,6 +56,7 @@ const DashCard = () => {
       cardBg: "bg-orange-100",
       hexBg: "bg-orange-500",
       icon: <FaBell />,
+      path: '/parentchildnotice'
     },
   ];
 
@@ -62,7 +65,8 @@ const DashCard = () => {
       {cardData.map((item, index) => (
         <div
           key={index}
-          className={`relative flex flex-col items-center justify-center w-[200px] p-4 rounded-lg shadow-sm ${item.cardBg}`}
+          className={`relative flex flex-col items-center justify-center w-[200px] p-4 rounded-lg shadow-sm ${item.cardBg} cursor-pointer`}
+          onClick={() => navigate(item?.path)}
         >
           {isLoading ? (
             <ParentDashcard />
@@ -79,7 +83,7 @@ const DashCard = () => {
                 {item.icon}
               </div>
               {/* Label and Value */}
-              <div className="text-gray-800 font-semibold text-sm">
+              <div className="text-gray-800 font-semibold text-sm text-center">
                 {item.label}
               </div>
               <div className="text-xl font-bold text-gray-700">
