a5d49ef18e081ccda88f943713828c0379cf4d8d
Aman Dubey <amandubey8833@gmail.com>
2025-06-23 15:26:35 +0530

    Feat : UI Enhancements | Student panel

--- src/Modules/Student/Dashboard/StudentMainSection.js
+++ src/Modules/Student/Dashboard/StudentMainSection.js
@@ -47,7 +47,7 @@ const StudentMainSection = () => {
     tasks,
     examResults: results,
   } = useSelector((state) => state.student.studentDashboard);
-console.log(cardData,"cardDatacardData")
+  console.log(cardData, "cardDatacardData");
   const { selectedClass, selectedSection } = useSelector(
     (state) => state?.common?.user?.classInfo
   );
@@ -78,47 +78,7 @@ console.log(cardData,"cardDatacardData")
       {/* Left Section */}
       <div className="w-[65%] px-4">
         <div className="flex flex-wrap justify-around  py-3 w-full">
-          {(cardData?.length
-            ? cardData
-            : [
-                // {
-                //   label: "Upcoming Exams",
-                //   value: 0,
-                //   bgColor: "bg-purple-100",
-                //   textColor: "text-black-500",
-                //   pentagonColor: "bg-purple-500", // Pentagon color matching bgColor
-                //   icon: <MdOutlineSchool className="text-xl text-white" />,
-                //   url: "/student_dash",
-                // },
-                {
-                  label: "Unpiad",
-                  value: 0,
-                  bgColor: "bg-red-100",
-                  textColor: "text-black-500",
-                  pentagonColor: "bg-red-500",
-                  icon: <MdAttachMoney className="text-xl text-white" />,
-                  url: "/student_finance",
-                },
-                {
-                  label: "Upcoming Events",
-                  value: 0,
-                  bgColor: "bg-blue-100",
-                  textColor: "text-black-500",
-                  pentagonColor: "bg-blue-500",
-                  icon: <MdEvent className="text-xl text-white" />,
-                  url: "/student/noticeboard/events",
-                },
-                {
-                  label: "Upcoming Notices",
-                  value: 0,
-                  bgColor: "bg-orange-100",
-                  textColor: "text-black-500",
-                  pentagonColor: "bg-orange-500",
-                  icon: <MdNotifications className="text-xl text-white" />,
-                  url: "/student/noticeboard/announcements",
-                },
-              ]
-          )?.map((item, index) => (
+          {cardData?.map((item, index) => (
             <DashCard key={index} {...item} />
           ))}
         </div>
